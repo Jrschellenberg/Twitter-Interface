4285d9628172dbd5e2b2e9b098792e73774f6a37
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-02-03 17:46:51 -0600

    Added twitter npm package, interacted with twitter api to get data req

--- src/app.js
+++ src/app.js
@@ -5,10 +5,45 @@
 
 const express = require('express');
 const app = express();
+const secrets = require('./config.json');
+const Twitter = require('twitter');
+
+var client = new Twitter({
+	consumer_key: secrets.consumerKey,
+	consumer_secret: secrets.consumerSecret,
+	access_token_key: secrets.accessToken,
+	access_token_secret: secrets.accessTokenSecret
+});
 
 app.get('/', function(req,res){
-	res.send("Hello World");
+	client.get('statuses/user_timeline', function(error, tweets, response){
+		console.log(tweets);
+		console.log("We got in here?");
+		res.send(tweets[0]);
+	});
+	//res.send("hello World.");
+	
 });
+
+app.get('/recent', function(req, res){
+	client.get('direct_messages', function(error, tweets, response){
+		res.send(tweets);
+		//res.send(response);
+		console.log(error);
+		console.log(response);
+	});
+});
+
+app.get('/friend', function(req, res){
+	client.get('friends/list', function(error, tweets, response){
+		res.send(tweets);
+		//res.send(response);
+		console.log(error);
+		console.log(response);
+	});
+});
+
+
 
 app.listen(3000, function(){
 	console.log("Front end listening on port 3000!");
