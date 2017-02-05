57f9f5ef02e6e466517ea7e3bf7ed27cd8b9be2c
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-02-05 12:39:29 -0600

    Starting on the views

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@
 
 const express = require('express');
 const app = express();
-const secrets = require('./config.json');
+const secrets = require('./config.js');
 const Twitter = require('twitter');
+
+app.use('/static', express.static(__dirname+'/public'));
 
 var client = new Twitter({
 	consumer_key: secrets.consumerKey,
@@ -15,31 +17,90 @@ var client = new Twitter({
 	access_token_secret: secrets.accessTokenSecret
 });
 
+var count = 5;
+
+
 app.get('/', function(req,res){
-	client.get('statuses/user_timeline', function(error, tweets, response){
-		console.log(tweets);
-		console.log("We got in here?");
-		res.send(tweets[0]);
+	
+	client.get('statuses/user_timeline',{count: count}, function(error, tweets, response){
+		console.log("hit request");
+		console.log(tweets.length);
+		if(!error && response.statusCode === 200) {
+			var array = [];
+			
+			
+			for(var i=0; i<tweets.length; i++) {
+				
+				
+				var object = {};
+				object.text = tweets[i].text;
+				object.userName = tweets[i].user.name;
+				object.screenName = tweets[i].user.screen_name;
+				object.profileImageURL = tweets[i].user.profile_image_url;
+				object.createdAt = tweets[i].created_at;
+				object.favoriteCount = tweets[i].favorite_count;
+				object.retweetCount = tweets[i].retweet_count;
+				
+				array.push(object);
+				
+				//console.log(tweets[i].text);
+				//console.log(tweets[i].user.name);
+				//console.log(tweets[i].user.screen_name);
+				//console.log(tweets[i].user.profile_image_url);
+				//console.log(tweets[i].created_at);
+				//console.log(tweets[i].favorite_count);
+				//console.log(tweets[i].retweet_count);
+				
+			}
+			var sentJSON = JSON.stringify(array);
+			res.send(sentJSON);
+			/*
+			 Going to need.
+			 tweets.text //the text
+			 tweets.user.name // the tweeters name
+			 tweets.user.screen_name // the tweeters screen name
+			 tweets.profile_image_url // the url to the background image.
+			 tweets.created_at // the time it is created, need to do some math to get hrs ago.
+			 tweets.favorite_count //the number of likes it has
+			 tweets.retweet_count //the number of times it has been retweeted.
+			 */
+			
+			
+			//res.send();
+		}//end if
+		else{
+			console.log("hitting else?");
+		}
 	});
 	//res.send("hello World.");
 	
 });
 
 app.get('/recent', function(req, res){
-	client.get('direct_messages', function(error, tweets, response){
+	client.get('direct_messages', {count: count}, function(error, tweets, response){
 		res.send(tweets);
 		//res.send(response);
 		console.log(error);
-		console.log(response);
+		//console.log(response);
 	});
 });
 
-app.get('/friend', function(req, res){
-	client.get('friends/list', function(error, tweets, response){
+app.get('/myrecent', function(req, res){
+	client.get('direct_messages/sent',{count: count}, function(error, tweets, response){
 		res.send(tweets);
 		//res.send(response);
 		console.log(error);
-		console.log(response);
+		//console.log(response);
+	});
+});
+
+
+app.get('/friend', function(req, res){
+	client.get('friends/list', {count: count}, function(error, tweets, response){
+		res.send(tweets);
+		//res.send(response);
+		console.log(error);
+		//console.log(response);
 	});
 });
 
