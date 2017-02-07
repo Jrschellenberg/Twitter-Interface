9116bf395fb55ea2586fa12ff1e1229eca0119f2
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-02-07 00:26:10 -0600

    Added async Parallel control flow to project. Also added recent FriendList

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@
 
 const express = require('express');
 const app = express();
+const async = require('async');
 const secrets = require('./config.js');
 const Twitter = require('twitter');
 
@@ -25,61 +26,70 @@ var count = 5;
 
 
 app.get('/', function(req,res){
-	
-	client.get('statuses/user_timeline',{count: count}, function(error, tweets, response){
-		console.log("hit request");
-		console.log(tweets.length);
-		if(!error && response.statusCode === 200) {
-			var array = [];
+	async.parallel({
+		timeLine: function(callback) {
+			client.get('statuses/user_timeline',{count: count}, function(error, tweets, response){
+				//console.log("hit request");
+				//console.log(tweets.length);
+				if(!error && response.statusCode === 200) {
+					var timeLineArray = [];
+					
+					for(var i=0; i<tweets.length; i++) {
+						var object = {};
+						var d = new Date(tweets[i].created_at);
+						object.text = tweets[i].text;
+						object.userName = tweets[i].user.name;
+						object.screenName = tweets[i].user.screen_name;
+						object.profileImageURL = tweets[i].user.profile_image_url;
+						object.createdAt = timeSince(d);
+						object.favoriteCount = tweets[i].favorite_count;
+						object.retweetCount = tweets[i].retweet_count;
+						object.friendCount = tweets[i].user.friends_count;
+						timeLineArray.push(object);
+					} // end for
+					//res.send(tweets[0]);
+					
+					//console.log(timeLineArray);
+					callback(null, timeLineArray);
+				}//end if
+				else{
+					console.log("hitting else?");
+					//Render an error here
+				}//end else
+			});//end client call
 			
-			
-			for(var i=0; i<tweets.length; i++) {
-				
-				
-				var object = {},
-				d = new Date(tweets[i].created_at);
-				
-				object.text = tweets[i].text;
-				object.userName = tweets[i].user.name;
-				object.screenName = tweets[i].user.screen_name;
-				object.profileImageURL = tweets[i].user.profile_image_url;
-				object.createdAt = timeSince(d);
-				object.favoriteCount = tweets[i].favorite_count;
-				object.retweetCount = tweets[i].retweet_count;
-				
-				
-				
-				array.push(object);
-				
-
-				
-			}
-
-			//res.send(tweets[0]);
-			
-			res.render('index', {timeLinePosts: array});
-		
-			
-			/*
-			 Going to need.
-			 tweets.text //the text
-			 tweets.user.name // the tweeters name
-			 tweets.user.screen_name // the tweeters screen name
-			 tweets.profile_image_url // the url to the background image.
-			 tweets.created_at // the time it is created, need to do some math to get hrs ago.
-			 tweets.favorite_count //the number of likes it has
-			 tweets.retweet_count //the number of times it has been retweeted.
-			 */
-			
-			
-			//res.send();
-		}//end if
-		else{
-			console.log("hitting else?");
+		},
+		friends: function(callback) {
+			client.get('friends/list', {count: count}, function(error, tweets, response){
+				if(!error && response.statusCode === 200) {
+					var friendsListArray = [];
+					for (var i = 0; i < tweets.users.length; i++) {
+						var object = {};
+						object.userName = tweets.users[i].name;
+						object.screenName = tweets.users[i].screen_name;
+						object.profileImageURL = tweets.users[i].profile_image_url;
+						object.following = tweets.users[i].following;
+						
+						friendsListArray.push(object);
+					} // end for
+					//res.send(tweets.users);
+					callback(null, friendsListArray);
+				}//end if
+				else{
+					console.log("Hitting an error here");
+					console.log(error);
+					console.log(response);
+				}//end else
+			});//end client call
 		}
-	});
-	//res.send("hello World.");
-	
+	}, function(err, results) {
+		if(!err){
+			console.log("Got into End of calls!");
+			//console.log(results.timeLine[0].friendCount);
+			res.render('index', {timeLinePosts: results.timeLine, 
+				friendsList: results.friends, friendCount: results.timeLine[0].friendCount});
+		}
+	});	
 });
 
 app.get('/recent', function(req, res){
@@ -102,7 +112,7 @@ app.get('/myrecent', function(req, res){
 
 
 app.get('/friend', function(req, res){
-	client.get('friends/list', {count: count}, function(error, tweets, response){
+	client.get('statuses/user_timeline', {count: count}, function(error, tweets, response){
 		res.send(tweets);
 		//res.send(response);
 		console.log(error);
@@ -145,3 +155,4 @@ function timeSince(date) {
 	}
 	return Math.floor(seconds) + "sec";
 }
+
