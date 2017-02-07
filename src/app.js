e2e8f9c49ec7f8b52eec503328137873f4418a97
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-02-07 17:50:34 -0600

    Some Reformatting. Adding Comments

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express();
 const async = require('async');
 const secrets = require('./config.js');
 const Twitter = require('twitter');
+const count = 5;
 
 app.use('/static', express.static(__dirname+'/public'));
 
@@ -22,17 +23,15 @@ var client = new Twitter({
 	access_token_secret: secrets.accessTokenSecret
 });
 
-var count = 5;
+
 
 
 app.get('/', function(req,res){
-	var errorMessage = "";
 	async.parallel({
 		timeLine: function(callback) {
 			client.get('statuses/user_timeline',{count: count}, function(error, tweets, response){
 				var timeLineArray = [];
 				if(!error && response.statusCode === 200) {
-					
 					
 					for(var i=0; i<tweets.length; i++) {
 						var object = {};
@@ -41,25 +40,19 @@ app.get('/', function(req,res){
 						object.userName = tweets[i].user.name;
 						object.screenName = tweets[i].user.screen_name;
 						object.profileImageURL = tweets[i].user.profile_image_url;
-						object.createdAt = timeSince(d);
+						object.createdAt = timeSince(d); //Calling timeSince function to calculate time since posted.
 						object.favoriteCount = tweets[i].favorite_count;
 						object.retweetCount = tweets[i].retweet_count;
 						object.friendCount = tweets[i].user.friends_count;
 						timeLineArray.push(object);
 					} // end for
-					//res.send(tweets[0]);
-					
-					//console.log(timeLineArray);
-					
 				}//end if
 				callback(error, timeLineArray);
 			});//end client call
-			
 		},
 		friends: function(callback) {
 			client.get('friends/list', {count: count}, function(error, tweets, response){
 				var friendsListArray = [];
-
 				if(!error && response.statusCode === 200) {
 					
 					for (var i = 0; i < tweets.users.length; i++) {
@@ -68,11 +61,8 @@ app.get('/', function(req,res){
 						object.screenName = tweets.users[i].screen_name;
 						object.profileImageURL = tweets.users[i].profile_image_url;
 						object.following = tweets.users[i].following;
-						
 						friendsListArray.push(object);
 					} // end for
-					//res.send(tweets.users);
-					
 				}//end if
 				callback(error, friendsListArray);
 			});//end client call
@@ -81,7 +71,10 @@ app.get('/', function(req,res){
 			var directMessageArray = [];
 			getDirectMessages('direct_messages');
 			getDirectMessages('direct_messages/sent');
-			
+			/*
+			A function closure since these calls are very similar.
+			@param call: The string to be passed to client.get refering to which api call to make.
+			 */
 			function getDirectMessages(call){
 				client.get(call, {count: count}, function(error, tweets, response){
 					if(!error && response.statusCode === 200) {
@@ -93,7 +86,7 @@ app.get('/', function(req,res){
 							object.screenName = tweets[i].sender.screen_name;
 							object.profileImageURL = tweets[i].sender.profile_image_url;
 							object.createdAt = tweets[i].created_at;
-							object.time = timeSince(d);
+							object.time = timeSince(d); //This is calling the function timeSince to convert to time since posted.
 							//Determining if it is the end user sending the message, or receiving. used for displaying view.
 							if(call ==='direct_messages/sent'){
 								object.me = true;
@@ -103,75 +96,40 @@ app.get('/', function(req,res){
 							}
 							directMessageArray.push(object);
 						} // end for
-						
-						//If the array is populated with the count amount *2 (for the 2 calls)
-						//Then this code executes
+						//If the array is populated with the count amount *2 (i.e on the 2nd call)
 						if(directMessageArray.length == (count*2)){
 							//Here we are sorting the array of messages based on date.
 							directMessageArray.sort(function(a,b){
 								return new Date(a.createdAt) - new Date(b.createdAt);
 							});
 							//Slice the array and keep only the amount up to count we want.
-							callback(error, directMessageArray.slice(count, directMessageArray.length));
+							callback(error, directMessageArray.slice(count, directMessageArray.length)); //giving only 5 recent messages to the callback
 						} // end if
 					} //end if
-					
-					//need to put callback here ????????
-					
 				});
 			}//end getDirectMessages Function.
 		}
 	}, function(err, results) {
-		console.log(err);
-		console.log(results.timeLine);
-		if(!err){
-			//console.log(results.timeLine[0].friendCount);
+		if(!err){ //Successfully requested all the api calls.
+			// Render the page with the populated information.
 			res.render('index', {timeLinePosts: results.timeLine, directMessages: results.directMessages,
-				friendsList: results.friends, friendCount: results.timeLine[0].friendCount});
+				friendsList: results.friends, friendCount: results.timeLine[0].friendCount, screenName: results.timeLine[0].screenName});
 		} //end if
 		else{
-			displayError(res, "There was an issue in handling your request.\n"+ err);
+			//Render the error page with the following hardString plus the error from twitter api.
+			res.render('error', {error: "There was an issue in handling your request.\n"+ err});
 		} //end else
 	});	// end function
-
-	
 }); // end async
-//
-//app.get('/recent', function(req, res){
-//	client.get('direct_messages', {count: count}, function(error, tweets, response){
-//		if(!error && response.statusCode === 200) {
-//			res.send(tweets);
-//			var directMessageArray = [];
-//			for (var i = 0; i < tweets.length; i++) {
-//				var object = {};
-//				object.text = tweets[i].text;
-//				object.userName = tweets[i].sender.name;
-//				object.screenName = tweets[i].sender.screen_name;
-//				object.profileImageURL = tweets[i].sender.profile_image_url;
-//				object.createdAt = tweets[i].created_at;
-//				directMessageArray.push(object);
-//			} // end for
-//		} //end if
-//		else{
-//			errorMessage("An error occured while requesting direct Messages");
-//		}//end else
-//	});
-//});
-//
-//app.get('/myrecent', function(req, res){
-//	client.get('direct_messages/sent',{count: count, since_id: '827662798922641400'}, function(error, tweets, response){
-//		res.send(tweets);
-//		//res.send(response);
-//		console.log(error);
-//		//console.log(response);
-//	});
-//});
-
 
 app.listen(3000, function(){
 	console.log("Front end listening on port 3000!");
 });
 
+/*
+A function to parse date object into amount of second, minute, hour, days, months, years ago since it was made.
+@param date: a Date object to compare against the current date.
+ */
 function timeSince(date) {
 	var seconds = Math.floor((new Date() - date) / 1000);
 	var interval = Math.floor(seconds / 31536000);
@@ -196,8 +154,3 @@ function timeSince(date) {
 	}
 	return Math.floor(seconds) + "sec";
 }
-
-function displayError(res, error){
-	res.render('error', {error: error});
-}
-
